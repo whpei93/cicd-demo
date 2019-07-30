667e0ff04d88fba4bd96baa40d09f150057868e0
wpei <wpei@cisco.com>
2019-07-30 14:40:08 +0800

    add pod name

--- cicd/web/views.py
+++ cicd/web/views.py
@@ -5,4 +5,5 @@ from django.http import HttpResponse
 
 def index(request):
     pod_name = os.environ.get('POD_NAME')
+    app_version = os.environ.get('APP_VERSION')
     return HttpResponse("%s: Hello, world. This is version 1.0" % pod_name)
