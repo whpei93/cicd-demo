555e038a53911e14ad2e7d77b913d0d9e6680724
wpei <wpei@cisco.com>
2019-08-05 14:22:20 +0800

    add app version

--- cicd/web/views.py
+++ cicd/web/views.py
@@ -6,4 +6,4 @@ from django.http import HttpResponse
 def index(request):
     pod_name = os.environ.get('POD_NAME')
     app_version = os.environ.get('APP_VERSION')
-    return HttpResponse("%s: Hello, world. This is version 1.0" % pod_name)
+    return HttpResponse("%s: Hello, world. This is version %s" %(pod_name, app_version))
